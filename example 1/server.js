ad3e8b13f8e9343172f84d55696767d85ee42ae4
Yoann LAMY <ynlamy@gmail.com>
2022-12-22 10:01:28 +0100

    Add files via upload

--- example 1/server.js
+++ example 1/server.js
@@ -5,11 +5,11 @@ const app = express();
 
 const PORT = 80;
 
-const os = require("os");
+const os = require('os');
 const hostname = os.hostname();
 
 app.get('/', (request, response) => {
-  response.send('Running on the container ' + String(hostname) + '\n');
+  response.send('Running on the container ' + hostname + '\n');
 });
 
 app.listen(PORT, () => {
