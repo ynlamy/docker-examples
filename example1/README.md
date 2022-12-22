3214f99f7d11779170879da6d4b4b4739381bb8d
Yoann LAMY <ynlamy@gmail.com>
2022-12-22 10:14:24 +0100

    Add files via upload

---
+++ example1/README.md
@@ -0,0 +1,19 @@
+# example1
+
+A simple example with Node.js on Docker.
+
+## Usage
+
+```
+docker build . -t <MyName>/<MyImage>:1.0.0
+docker images
+
+docker container run -d -p 8080:80 <MyName>/<MyImage>:1.0.0
+docker ps
+docker logs <ID>
+
+curl -i localhost:8080
+
+docker stop <ID>
+docker rm <ID>
+```
---
+++ example1/server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = 80;
+
+const os = require('os');
+const hostname = os.hostname();
+
+app.get('/', (request, response) => {
+  response.send('Running on the container ' + hostname + '\n');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
